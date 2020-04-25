c24b20fe45a4ffd889e115c13646678015d28401
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-04-25 17:28:55 +0200

    format

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,43 +1,39 @@
-
 <script>
+  let showMenu = false;
 
-let showMenu = false;
+  let disp = "";
 
-let disp = ''
+  let menuWrapper = "menuWrapper";
 
-let menuWrapper = "menuWrapper"
-
-function test(e) {
-  console.log('hey there form test')
-}
-function toggleMenu() {
-  console.log('toggling')
-  if (showMenu) {
-    console.log('hiding')
-    hide()
-  } else {
-    console.log('showing')
-    show()
+  function test(e) {
+    console.log("hey there form test");
   }
-  showMenu = !showMenu
-}
+  function toggleMenu() {
+    console.log("toggling");
+    if (showMenu) {
+      console.log("hiding");
+      hide();
+    } else {
+      console.log("showing");
+      show();
+    }
+    showMenu = !showMenu;
+  }
 
-function show() {
-  disp = " show"
-  menuWrapper += " active"
-}
+  function show() {
+    disp = " show";
+    menuWrapper += " active";
+  }
 
-function hide() {
-  showMenu = false
-  disp = ""
-  menuWrapper = "menuWrapper"
-}
-
+  function hide() {
+    showMenu = false;
+    disp = "";
+    menuWrapper = "menuWrapper";
+  }
 </script>
 
 <style>
-
-.menuWrapper {
+  .menuWrapper {
     width: 100%;
     border-bottom: 3px solid #1d3244;
     position: fixed;
@@ -45,16 +41,14 @@ function hide() {
     z-index: 10;
     background-color: white;
     box-shadow: 0 3px 11px #00000040;
-}
+  }
 
-
-.menu {
+  .menu {
     display: flex;
     align-items: center;
     max-width: 1500px;
     margin: 0 auto;
     padding: 10px 30px;
-    
   }
 
   .menu-logo {
@@ -120,12 +114,12 @@ function hide() {
   }
 
   @media only screen and (max-width: 768px) {
-  /* For mobile phones: */
+    /* For mobile phones: */
 
-  .active {
-    border-bottom: none;
-    box-shadow: 0 3px 11px #000000cc;
-  }
+    .active {
+      border-bottom: none;
+      box-shadow: 0 3px 11px #000000cc;
+    }
 
     .menu-logo {
       margin: 0;
@@ -133,91 +127,85 @@ function hide() {
 
     .menu {
       flex-direction: column;
-      padding: 1em 0 0 0 ;
-      
+      padding: 1em 0 0 0;
     }
 
+    .menu-mobile-button {
+      display: block;
+      position: absolute;
+      right: 1em;
+      top: 1em;
+    }
 
-  .menu-mobile-button {
-    display: block;
-    position: absolute;
-    right: 1em;
-    top: 1em;
+    .nav-list {
+      width: 100%;
+      flex-direction: column;
+      display: none;
+      /*border-top: 3px solid #d4d4d4; => Works pretty well with just background color*/
+    }
+
+    .item {
+      /*border-top: 3px solid #d4d4d4; => Works pretty well with just background color => adding more padding to compensate for missing whitespace*/
+      margin: auto;
+      background: #1d3244;
+      color: white;
+    }
+
+    .item span {
+      padding: 0.4em 0.8em 0.4em 0.8em;
+      text-align: center;
+      margin: auto;
+    }
+
+    .nav-list-wrapper {
+      width: 100%;
+    }
+
+    .nav-list:last-child .item:last-child {
+    }
   }
-
-  .nav-list {
-    width: 100%;
-    flex-direction: column;
-    display: none;
-    /*border-top: 3px solid #d4d4d4; => Works pretty well with just background color*/
-  }
-
-.item {
-  /*border-top: 3px solid #d4d4d4; => Works pretty well with just background color => adding more padding to compensate for missing whitespace*/
-  margin: auto;
-  background: #1d3244;
-  color: white;
-
-}
-
-
-  .item span {
-    padding: .4em .8em .4em .8em;
-    text-align: center;
-    margin: auto;
-  }
-
-  .nav-list-wrapper {
-    width: 100%;
-  }
-
-  .nav-list:last-child .item:last-child {
-    
-  }
-  }
-
 </style>
 
 <div class={menuWrapper}>
-    <div class="menu">
-        <div class="menu-logo">
-        <a href="#" class="menu-logo-a">
-            <img class="menu-logo-img" src={'logo.png'} />
-        </a>
-        </div>
-        <div class="menu-mobile-button" on:click={toggleMenu}>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-        </div>
-        <div class="nav-list-wrapper">
-        <div class={"nav-list" + disp} on:click={hide}>
-            <a href="/page/program">
-            <div class="item">
-                <span>PROGRAM</span>
-            </div>
-            </a>
-            <a href="/page/info">
-            <div class="item">
-                <span>INFO</span>
-            </div>
-            </a>
-            <a>
-            <div class="item">
-                <span>IDRETTER</span>
-            </div>
-            </a>
-            <a>
-            <div class="item">
-                <span>FRIVILLIGE</span>
-            </div>
-            </a>
-            <a>
-            <div class="item">
-                <span>OM STUDENTLEKENE</span>
-            </div>
-            </a>
-        </div>
-        </div>
+  <div class="menu">
+    <div class="menu-logo">
+      <a href="#" class="menu-logo-a">
+        <img class="menu-logo-img" src={'logo.png'} />
+      </a>
     </div>
+    <div class="menu-mobile-button" on:click={toggleMenu}>
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+    </div>
+    <div class="nav-list-wrapper">
+      <div class={'nav-list' + disp} on:click={hide}>
+        <a href="/page/program">
+          <div class="item">
+            <span>PROGRAM</span>
+          </div>
+        </a>
+        <a href="/page/info">
+          <div class="item">
+            <span>INFO</span>
+          </div>
+        </a>
+        <a>
+          <div class="item">
+            <span>IDRETTER</span>
+          </div>
+        </a>
+        <a>
+          <div class="item">
+            <span>FRIVILLIGE</span>
+          </div>
+        </a>
+        <a>
+          <div class="item">
+            <span>OM STUDENTLEKENE</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
